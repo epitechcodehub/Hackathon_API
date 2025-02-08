254e9ee318d66960702f9f73b142525f4e0b6b67
Iyad Hossen <iyad.hossen@epitech.eu>
2025-02-08 20:49:08 +0400

    Never announce your moves before you make them

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const url = "mongodb://localhost:27017/dbase"
 const app = express()
-const PORT = 8080
+const PORT = 8801
 
 app.use(express.json())
 
@@ -22,7 +22,15 @@ const UserSchema = new mongoose.Schema({
   age: Number
 })
 
+const AnnouncementScheme = new mongoose.Schema({
+  title: String,
+  description: String,
+  taken: Boolean
+})
+
 const UserModel = mongoose.model("users", UserSchema)
+const AnnouncementModel = mongoose.model("announcements", AnnouncementScheme)
+
 
 app.get("/API/getUsers", (req, res) => {
   UserModel.find({}).then(users => {
@@ -43,7 +51,7 @@ app.get("/API/getUsers/:name", (req, res) => {
 app.post("/API/addUsers", (req, res) => {
   const {name, lastname, mail, age} = req.body
   UserModel.create({name, lastname, mail, age}).then(user => {
-    res.status(201).send(`New user has been successfully created:\n${user}`)
+    res.status(201).send(user)
   }).catch(err => {
     res.status(400).send(err)
   })
@@ -53,8 +61,55 @@ app.delete("/API/delUsers/:id", (req, res) => {
   UserModel.findByIdAndDelete(req.params.id).exec().then(doc => {
     if (!doc)
       return res.status(404).send("The document was not found")
-    res.status(204).send("The document was found")
+    res.status(204).send(doc)
   }).catch(err => {
-    res.status(500).send(err)
+    res.status(404).send(err)
+  })
+})
+
+app.get("/API/getAnnouncements", (req, res) => {
+  AnnouncementModel.find({}).then(announcements => {
+    res.status(200).send(announcements)
+  }).catch(err => {
+    res.status(404).send(err)
+  })
+})
+
+app.get("/API/getAnnouncements/:title", (req, res) => {
+  AnnouncementModel.find(req.params).then(announcements => {
+    res.status(200).send(announcements)
+  }).catch(err => {
+    res.status(404).send(err)
+  })
+})
+
+app.post("/API/addAnnouncements", (req, res) => {
+  const {title, description, taken} = req.body
+  AnnouncementModel.create({title, description, taken}).then(announcement => {
+    res.status(201).send(announcement)
+  }).catch(err => {
+    res.status(400).send(err)
+  })
+})
+
+app.delete("/API/delAnnouncements/:id", (req, res) => {
+  AnnouncementModel.findByIdAndDelete(req.params.id).exec().then(doc => {
+    if (!doc)
+      return res.status(404).send("The document was not found")
+    res.status(204).send(doc)
+  }).catch(err => {
+    res.status(404).send(err)
+  })
+})
+
+app.put("/API/modifAnnouncements/:id", (req, res) => {
+  var conditions = {_id: req.params.id}
+  const taken = req.body
+  AnnouncementModel.updateOne(conditions, taken).then(doc => {
+    if (!doc)
+      return res.status(404).send("The document was not found")
+    res.status(200).send(doc)
+  }).catch(err => {
+    res.status(404).send(err)
   })
 })
